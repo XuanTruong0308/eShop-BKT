5094838d50021f7bb584738527712cce376ff8ba
ChunTapCode <dieuan2k01@gmail.com>
2026-06-26 11:01:31 +0700

    Save all feature updates

---
+++ src/eShop_flutter/apps/mobile_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Admin\\develop\\flutter_windows_3.44.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Admin\\develop\\flutter_windows_3.44.2-stable\\flutter"
+  "PROJECT_DIR=D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app"
+  "FLUTTER_ROOT=C:\\Users\\Admin\\develop\\flutter_windows_3.44.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app"
+  "FLUTTER_TARGET=D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzlhNmM0ODQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzdlMmU5NDc3Mg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Subagent\\Nhom_10_DevOps_Testing_Production\\eShop-main\\src\\eShop_flutter\\apps\\mobile_app\\.dart_tool\\package_config.json"
+)
